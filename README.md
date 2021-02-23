87a34e34c6e73d577cb782432e9a1ee827a82348
trungnttb <trungnttb.it@gmail.com>
2021-02-23 16:06:46 +0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# dynamic-nested-sidenav-menu-tavwfs
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/dynamic-nested-sidenav-menu-tavwfs)
---
+++ app/nav.service.ts
@@ -0,0 +1,27 @@
+import {EventEmitter, Injectable} from '@angular/core';
+import {Event, NavigationEnd, Router} from '@angular/router';
+import {BehaviorSubject} from 'rxjs';
+
+@Injectable()
+export class NavService {
+  public appDrawer: any;
+  public currentUrl = new BehaviorSubject<string>(undefined);
+
+  constructor(private router: Router) {
+    this.router.events.subscribe((event: Event) => {
+      
+      if (event instanceof NavigationEnd) {
+        console.log(1)
+        this.currentUrl.next(event.urlAfterRedirects);
+      }
+    });
+  }
+
+  public closeNav() {
+    this.appDrawer.close();
+  }
+
+  public openNav() {
+    this.appDrawer.open();
+  }
+}
